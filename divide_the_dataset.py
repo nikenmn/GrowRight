b581f6618198f8535fa22787acee0d681cea0bb8
Niken Maharani <nikenmaharani24@gmail.com>
2024-05-27 22:34:32 +0700

    dividing the dataset to training and testing

--- divide_the_dataset.py
+++ divide_the_dataset.py
@@ -4,15 +4,15 @@ import pandas as pd
 data = pd.read_csv('Stunting_cleanedAndFix.csv')
 
 # Separate 70% of the data
-separation_index = int(len(data) * 0.7)
-data_70 = data[:separation_index]
+separation_index = int(len(data) * 0.8)
+data_80 = data[:separation_index]
 
 # Separate the rest 30% of the data
-data_30 = data[separation_index:]
+data_20 = data[separation_index:]
 
 # Delete the Stunting column from the 30% data
-data_30 = data_30.drop('Stunting', axis=1)
+data_20 = data_20.drop('Stunting', axis=1)
 
 # Save the separated data to new CSV files
-data_70.to_csv('Stunting_70.csv', index=False)
-data_30.to_csv('Stunting_30.csv', index=False)
+data_80.to_csv('Stunting_Training.csv', index=False)
+data_20.to_csv('Stunting_Testing.csv', index=False)
