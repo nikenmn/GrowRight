fa4575a8d8d2be3bcb66076ada0952b8f6d04f98
AryKurnia <aryk922@gmail.com>
2024-06-09 19:30:34 +0800

    fixt: connection database configuration

--- src/config/database.js
+++ src/config/database.js
@@ -1,14 +1,45 @@
 require('dotenv').config();
-const MySQL = require('mysql2/promise');
+const { Connector } = require('@google-cloud/cloud-sql-connector');
+const mysql = require('mysql2/promise');
 
-const db = MySQL.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-});
+// Mendefinisikan fungsi untuk menentukan tipe IP
+const getIpType = () => (
+  process.env.PRIVATE_IP === '1' || process.env.PRIVATE_IP === 'true'
+    ? 'PRIVATE'
+    : 'PUBLIC'
+);
 
+// Fungsi untuk menginisialisasi koneksi dengan Google Cloud SQL Connector
+const connectWithConnector = async (config) => {
+  const connector = new Connector();
+  const clientOpts = await connector.getOptions({
+    instanceConnectionName: process.env.INSTANCE_CONNECTION_NAME,
+    ipType: getIpType(),
+  });
+  const dbConfig = {
+    ...clientOpts,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    ...config,
+  };
+  return mysql.createPool(dbConfig);
+};
+
+// Menginisialisasi koneksi ke database
+const initializeDb = async () => {
+  try {
+    const db = await connectWithConnector({});
+    return db;
+  } catch (err) {
+    console.error('Error initializing the database connection:', err.stack);
+    throw err;
+  }
+};
+
+// Memeriksa koneksi ke database
 const cekDbConnection = async () => {
+  const db = await initializeDb();
   try {
     const connection = await db.getConnection();
     console.log('DB is connected');
@@ -20,4 +51,4 @@ const cekDbConnection = async () => {
 
 cekDbConnection();
 
-module.exports = db;
+module.exports = initializeDb;
